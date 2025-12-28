a6a83a11c78d8555eeb238f32058c23695a81034
iamrayghazali <151096230+iamrayghazali@users.noreply.github.com>
2025-12-28 20:53:06 +0100

    Added business card enums

---
+++ frontend/src/enums/BusinessCardEnums.ts
@@ -0,0 +1,23 @@
+export enum BusinessCardBackground {
+    None,
+    url
+}
+
+// color price
+export enum BusinessCardColor {
+    Black = 2,
+    Green = 5,
+    White = 0,
+}
+
+// layout price
+export enum BusinessCardLayout {
+    Classic = 0,
+    Standard = 1,
+    Default = 2,
+}
+
+// base product price
+export const ProductBasePrice = {
+    BusinessCard: 10,
+};
