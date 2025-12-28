f3d028bcc407f40f351f115bbc9fa0ba73e15665
iamrayghazali <151096230+iamrayghazali@users.noreply.github.com>
2025-12-28 20:52:52 +0100

    Added new paths

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,23 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import NotFound from "./pages/NotFound";
 import "./index.css";
+import Products from "./pages/Products";
+import BusinessCard from "./pages/products/BusinessCard.tsx";
+import {CartProvider} from "./context/CartContext.tsx";
+import Cart from "./pages/Cart.tsx";
+import Canvas from "./pages/products/Canvas.tsx";
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
+  { path: "/products", element: <Products/>},
+    { path: "/cart", element: <Cart/>},
+    { path: "/products/businesscard", element: <BusinessCard/>},
+    { path: "/products/canvas", element: <Canvas/>},
   { path: "*", element: <NotFound /> },
 ]);
 
 createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />
+    <CartProvider>
+        <RouterProvider router={router} />
+    </CartProvider>
 );
